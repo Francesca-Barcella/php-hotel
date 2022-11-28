42fc2b36110a4e6020ae97df81b51932c6459c34
Francesca-Barcella <pollon.fra@gmail>
2022-11-28 17:33:41 +0100

    php-hotels - info in tabella

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
 Stampare tutti i nostri hotel con tutti i dati disponibili.
 FATTO - Iniziate in modo graduale. Prima stampate in pagina i dati, senza preoccuparvi dello stile.
-Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+FATTO - Dopo aggiungete Bootstrap
+FATTO - mostrate le informazioni con una tabella.
 Bonus: 1
 Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
 Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
@@ -66,35 +67,56 @@ $hotels = [
     <title>Hotels</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
-    <h1>elenco hotel e servizi</h1>
-
-    <div>
-        <ul>
-            <!-- metto l'apertura del foreach -->
+    <div class="container">
+        <div class="row">
             <?php foreach ($hotels as $hotel) : ?>
-                <!-- lista con nome e servizi dei vari hotle -->
-                <div>Nome dell'hotel: <strong><?php echo $hotel['name'] ?></strong></div>
-                <div>Descrizione dell'hotel: <?php echo $hotel['description'] ?></div>
-                <div>Presenza del parcheggio:
-                    <!-- se parking === true stampa "presente" altrimenti stamp "assente" -->
+                <!-- nome hotel -->
+                <h1 class="text-center text-black-75 bg-primary bg-gradient bg-opacity-50 p-3"><?php echo $hotel['name'] ?></h1>
+                <!-- descrizione hotel -->
+                <div class="col p-3 text-center bg-dark bg-opacity-25">
+                    <div><strong>Descrizione dell'hotel</strong></div>
+                    <?php echo $hotel['description'] ?>
+                </div>
+                <!-- parcheggio hotel -->
+                <div class="col p-3 text-center bg-secondary bg-opacity-25">
+                    <div><strong>Parcheggio</strong></div>
+
                     <?php
                     if ($hotel['parking'] === true) {
-                        echo 'presente';
+                        echo 'Presente';
                     } else {
-                        echo 'assente';
+                        echo 'Assente';
                     }
                     ?>
                 </div>
-                <div>Valutazione dell'hotel: <?php echo $hotel['vote'] ?></strong></div>
-                <div>Distanza dal centro: <?php echo $hotel['distance_to_center'] ?> km</div>
-
-                <!-- metto la chiusura del foreach -->
+                <!-- vautazione hotel -->
+                <div class="col p-3 text-center bg-dark bg-opacity-25">
+                    <div><strong>Valutazione dell'hotel</strong></div>
+                    <?php
+                    if ($hotel['vote'] === 1) {
+                        echo '★';
+                    } elseif ($hotel['vote'] === 2) {
+                        echo '★★';
+                    } elseif ($hotel['vote'] === 3) {
+                        echo '★★★';
+                    } elseif ($hotel['vote'] === 4) {
+                        echo '★★★★';
+                    } elseif ($hotel['vote'] === 5) {
+                        echo '★★★★★';
+                    }
+                    ?>
+                </div>
+                <!-- distanza hotel dal centro-->
+                <div class="col p-3 text-center bg-secondary bg-opacity-25">
+                    <div><strong>Distanza dal centro</strong></div>
+                    <?php echo $hotel['distance_to_center'] ?> km
+                </div>
             <?php endforeach; ?>
-
-        </ul>
+        </div>
     </div>
 
 </body>
