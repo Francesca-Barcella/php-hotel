b8d0533c87adcc06cca39a67a8d81153e489b5ef
Francesca-Barcella <pollon.fra@gmail>
2022-11-28 16:13:18 +0100

    php-hotels - stampa in pagina di tutti i dettagli

--- index.php
+++ index.php
@@ -1,43 +1,90 @@
 <?php
+/* TRACCIA
 
-    $hotels = [
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale. Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+Bonus: 1
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+ */
 
-    ];
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+//var_dump($hotels)
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+</head>
+
+<body>
+    <h1>elenco hotel e servizi</h1>
+
+    <div>
+        <ul>
+            <!-- metto l'apertura del foreach -->
+            <?php foreach ($hotels as $hotel) : ?>
+                <!-- lista con nome e servizi dei vari hotle -->
+                <li> Nome dell'hotel: <strong><?php echo $hotel['name'] ?></strong></li>
+                <li> Descrizione dell'hotel: <?php echo $hotel['description'] ?> </li>
+                <li> Presenza del parcheggio: <?php echo $hotel['parking'] ?> </li>
+                <li> Valutazione dell'hotel: <?php echo $hotel['vote'] ?> </li>
+                <li> Nome dell'hotel:<?php echo $hotel['distance_to_center'] ?> </li>
+            <!-- metto la chiusura del foreach -->
+            <?php endforeach; ?>
+
+        </ul>
+    </div>
+
+</body>
+
+</html>
